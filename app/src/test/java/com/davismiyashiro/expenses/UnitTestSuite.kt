05de6479a6ab7696291a4927ac2eb501a38cbeff
Davis Miyashiro <davis.miyashiro@gmail.com>
2018-05-25 14:19:55 +0100

    Initial kotlin commit

---
+++ app/src/test/java/com/davismiyashiro/expenses/UnitTestSuite.kt
@@ -0,0 +1,14 @@
+package com.davismiyashiro.expenses
+
+import com.davismiyashiro.expenses.view.managetabs.ChooseTabsPresenterImplTest
+import com.davismiyashiro.expenses.model.InMemoryTabsRepositoryTest
+
+import org.junit.runner.RunWith
+import org.junit.runners.Suite
+
+/**
+ * Created by Davis on 30/12/2015.
+ */
+@RunWith(Suite::class)
+@Suite.SuiteClasses(InMemoryTabsRepositoryTest::class, ChooseTabsPresenterImplTest::class)
+class UnitTestSuite
---
+++ app/src/test/java/com/davismiyashiro/expenses/datatypes/TabTest.kt
@@ -0,0 +1,17 @@
+package com.davismiyashiro.expenses.datatypes
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Created by Davis Miyashiro on 07/12/2016.
+ */
+class TabTest {
+    @Test
+    @Throws(Exception::class)
+    fun equals() {
+
+    }
+
+}
