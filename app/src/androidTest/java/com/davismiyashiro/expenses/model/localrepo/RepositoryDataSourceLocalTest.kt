fedc6f15e02d800e7ff0cd769f1474427a1f4d4e
Davis Miyashiro <davis.miyashiro@gmail.com>
2019-04-13 14:20:05 +0100

    Code clean up

--- app/src/androidTest/java/com/davismiyashiro/expenses/model/localrepo/RepositoryDataSourceLocalTest.kt
+++ app/src/androidTest/java/com/davismiyashiro/expenses/model/localrepo/RepositoryDataSourceLocalTest.kt
@@ -1,3 +1,26 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2019 Davis Miyashiro
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 package com.davismiyashiro.expenses.model.localrepo
 
 import android.support.test.InstrumentationRegistry.getTargetContext
@@ -93,7 +116,6 @@ class TabRepositoryDataSourceLocalTest {
                                 database?.deleteTab(tabs.groupId)
                             }
                         })
-
                     } else {
                         assertNotEquals(groupName, TAB_NAME)
                     }
@@ -144,7 +166,7 @@ class TabRepositoryDataSourceLocalTest {
 
         database?.getAllParticipants(PART_TABID, object : RepositoryDataSource.ParticipantServiceCallback<ArrayMap<String, Participant>> {
             override fun onLoaded(participants: ArrayMap<String, Participant>) {
-                assertNotNull(participants);
+                assertNotNull(participants)
                 assertThat(participants.size, `is`(0))
             }
 
@@ -190,7 +212,7 @@ class TabRepositoryDataSourceLocalTest {
 
         database?.getAllExpenses(EXP_TABID, object : RepositoryDataSource.ExpenseServiceCallback<ArrayMap<String, Expense>> {
             override fun onLoaded(expenses: ArrayMap<String, Expense>) {
-                assertNotNull(expenses);
+                assertNotNull(expenses)
                 assertThat(expenses.size, `is`(0))
             }
 
